dd6202979a362381616bde54638fbc190d92ee00
Maanasa64 <maanasaprasad99@gmail.com>
2025-04-10 23:21:06 -0700

    diff case

--- src/main.c
+++ src/main.c
@@ -5,17 +5,18 @@
 int main() {
     // Bytecode program: PUSH 5, PUSH 10, ADD, HALT
         uint8_t code[] = {
-            OP_PUSH, 5, 0, 0, 0,    // PUSH 5 (little-endian)
-            OP_PUSH, 10, 0, 0, 0,   // PUSH 10 (little-endian)
-            OP_ADD,
-            OP_HALT
-        };
-
+        OP_PUSH, 5, 0, 0, 0,
+        OP_PUSH, 3, 0, 0, 0,
+        OP_ADD,
+        OP_PUSH, 2, 0, 0, 0,
+        OP_MUL,
+        OP_PUSH, 4, 0, 0, 0,
+        OP_SUB,
+        OP_HALT
+    };
 
     VM *vm = vm_create(code);
     vm_execute(vm);
-
     printf("Result: %d\n", vm_pop(vm));
     vm_free(vm);
-    return 0;
 }
